00fc9c5e33f03d985c6a8fb53fa1a72db5ec6b8d
Carla Cairns <atl-carlac@corp.expeditors.com>
2022-10-11 14:33:06 -0500

    refcatored to created react compontents

--- staticReactPage/index.js
+++ staticReactPage/index.js
@@ -1,18 +1,31 @@
-const react_logo = (
-  <nav>
-    <img src="./react-logo.png" alt="react logo" width="40px"></img>
-  </nav>
-);
-const header = <h1>Fun Facts about React</h1>;
-const react_facts = (
-  <ul>
-    <li>Was first released in 2013</li>
-    <li>Was originally created by Jordan Walke</li>
-    <li>Has well over 100k stars on Github</li>
-    <li>Is maintained by Facebook</li>
-    <li>Powers thousands of enterprise apps, including mobile apps</li>
-  </ul>
-);
+function React_logo() {
+  return (
+    <nav>
+      <img src="./react-logo.png" alt="react logo" width="40px"></img>
+    </nav>
+  );
+}
+function Page_header() {
+  return <h1>Fun Facts about React</h1>;
+}
+function React_facts() {
+  return (
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  );
+}
 
 const root = document.getElementById("root");
-ReactDOM.render([react_logo, header, react_facts], root);
+ReactDOM.render(
+  <div>
+    <React_logo />
+    <Page_header />
+    <React_facts />
+  </div>,
+  root
+);

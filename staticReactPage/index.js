69377c7dab729ac2b9db6a5a138ae4245da69468
Carla Cairns <atl-carlac@corp.expeditors.com>
2022-10-11 14:27:17 -0500

    basic react static page

--- staticReactPage/index.js
+++ staticReactPage/index.js
@@ -1,92 +1,18 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// install dependencies and get rid of text/babel in script
-function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
-            </a>
-          </li>
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </div>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="#">
-              Disabled
-            </a>
-          </li>
-        </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button
-            className="btn btn-outline-success my-2 my-sm-0"
-            type="submit"
-          >
-            Search
-          </button>
-        </form>
-      </div>
-    </nav>
-  );
-}
-function Title() {
-  return <h1>I'm learing React!</h1>;
-}
-ReactDOM.render(
-  <div>
-    <Navbar />
-    <Title />
-  </div>,
-  document.getElementById("root")
+const react_logo = (
+  <nav>
+    <img src="./react-logo.png" alt="react logo" width="40px"></img>
+  </nav>
 );
+const header = <h1>Fun Facts about React</h1>;
+const react_facts = (
+  <ul>
+    <li>Was first released in 2013</li>
+    <li>Was originally created by Jordan Walke</li>
+    <li>Has well over 100k stars on Github</li>
+    <li>Is maintained by Facebook</li>
+    <li>Powers thousands of enterprise apps, including mobile apps</li>
+  </ul>
+);
+
+const root = document.getElementById("root");
+ReactDOM.render([react_logo, header, react_facts], root);
